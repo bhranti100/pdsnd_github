b622115a30cd5f24b0861bfdc0e64521ae6912b2
bhranti <bhranti.patel@gmail.com>
2018-11-03 20:14:12 -0400

    created and imported constants

---
+++ constants.py
@@ -0,0 +1,5 @@
+CITY_DATA = { 'chicago': 'chicago.csv',
+              'new york city': 'new_york_city.csv',
+              'washington': 'washington.csv' }
+MONTH = ['all', 'january', 'february', 'march', 'april', 'may', 'june']
+WEEK = ['all', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
